61410fc926ee4df5dadda860ec54fbe6563d580a
Richard Gichuki <chadgichuki@gmail.com>
2022-08-20 13:22:39 +0300

    commit tree 5bc63dd32d29c7cadbc75bf6d10aaab44da9230e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_accessor :x, :y, :z
+  def initialize(x,y,z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+    if self.x <=0 || self.y <=0 || self.z <=0
+      raise TriangleError
+    elsif self.x >= (self.y + self.z) || self.y >= (self.x + self.z) || self.z >= (self.x + self.y)
+      raise TriangleError
+    elsif self.x == self.y && self.x == self.z
+      return :equilateral
+    elsif self.x == self.y || self.x == self.z || self.y == self.z
+      return :isosceles
+    else
+      return :scalene
+    end
+
+    
+  end
+
+  class TriangleError < StandardError
+    def message
+      puts "Inalid Triangle"
+    end
+  end
 end
